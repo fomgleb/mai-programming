f5f2890a0192a12e9e229a5992dcc871f19f7d43
fomgleb <fomglib@gmail.com>
2023-09-18 11:05:53 +0300

    Add info prining if test fails

--- lecture2/test/test_foo_string.cpp
+++ lecture2/test/test_foo_string.cpp
@@ -1,17 +1,25 @@
-#include "cassert"
 #include "foo_string.h"
+#include <iostream>
 
-void assert_length(const char *const, const int);
+#define ASSERT(condition) \
+    do { \
+        if (!(condition)) { \
+            std::cout << "Assertion failed: (" #condition "), function " << __FUNCTION__ \
+                        << ", file " << __FILE__ << ", line " << __LINE__ << "." << std::endl; \
+            std::abort(); \
+        } \
+    } while (false)
+
+#define ASSERT_LENGTH(str, len) \
+    do { \
+        FooString foo_string = FooString((char *)str); \
+        ASSERT(foo_string.length() == len); \
+    } while (false)
 
 int main() {
-    assert_length("Hello world", 12);
-    assert_length(" ", 2);
-    assert_length("", 1);
+    ASSERT_LENGTH("Hello world", 12);
+    ASSERT_LENGTH(" ", 2);
+    ASSERT_LENGTH("", 1);
 
     return 0;
-}
-
-void assert_length(const char *const string, const int length) {
-    FooString foo_string = FooString((char *)string);
-    assert(foo_string.length() == length);
 }
