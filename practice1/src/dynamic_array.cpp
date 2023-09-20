a0142eab20e959c833a2f36e6e0d74f7995c417a
fomgleb <fomglib@gmail.com>
2023-09-20 15:52:58 +0300

    practice1: Initialize array elements to zero

--- practice1/src/dynamic_array.cpp
+++ practice1/src/dynamic_array.cpp
@@ -1,7 +1,7 @@
 #include "dynamic_array.hpp"
 
 DynamicArray::DynamicArray(size_t size) : size(size) {
-    array = new int8_t[size];
+    array = new int8_t[size]();
 }
 
 DynamicArray::~DynamicArray() {
