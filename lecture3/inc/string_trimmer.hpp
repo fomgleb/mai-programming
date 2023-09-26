dcf0520fa3767c9c6c461e07835af7e6bb680f6a
fomgleb <fomglib@gmail.com>
2023-09-26 17:09:28 +0300

    lecture3: Add functions for string trimming

---
+++ lecture3/inc/string_trimmer.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <algorithm> 
+#include <cctype>
+#include <locale>
+
+void ltrim(std::string &s);
+void rtrim(std::string &s);
+void trim(std::string &s);
+std::string ltrim_copy(std::string s);
+std::string rtrim_copy(std::string s);
+std::string trim_copy(std::string s);
