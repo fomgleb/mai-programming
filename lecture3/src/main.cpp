43f469f1e16fbbdd364abe6d62ab560cfa757042
fomgleb <fomglib@gmail.com>
2023-09-26 18:50:57 +0300

    lecture3: Add example of using `Gift` class

--- lecture3/src/main.cpp
+++ lecture3/src/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include "gift.hpp"
 
 int main() {
-    std::cout << "Test";
+    std::ofstream outputFile("output.txt", std::ios::out);
+    Gift gift("input.gift");
+    for (auto question : gift.getQuestions()) {
+        outputFile << question->getText() << std::endl;
+        vector<string> answers = question->getAnswers();
+        for (size_t i = 0; i < answers.size(); i++) {
+            outputFile << (i == question->getCorrectAnswerIndex() ? '+' : '-')
+                << " " << answers[i] << std::endl;
+        }
+        outputFile << std::endl;
+    }
     return 0;
 }
