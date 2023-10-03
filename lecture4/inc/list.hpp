a31b1890a417b5a0927788c8a71e0adb0c088104
fomgleb <fomglib@gmail.com>
2023-10-03 16:11:52 +0300

    lecture4: Add `List::Insert` method

--- lecture4/inc/list.hpp
+++ lecture4/inc/list.hpp
@@ -18,6 +18,20 @@ public:
         }
     }
     size_t GetSize() { return size; }
+    void Insert(T newElement, size_t index) {
+        size_t currIndex = 0;
+        Node* currNode = head;
+        Node* prevNode = nullptr;
+        while (currNode != nullptr && currIndex != index) {
+            currIndex++;
+            prevNode = currNode;
+            currNode = currNode->next;
+        }
+
+        Node* newNode = new Node {currNode, newElement};
+        prevNode == nullptr ? (head = newNode) : (prevNode->next = newNode);
+        size++;
+    }
     struct iterator {
         Node* current;
         iterator(Node* node) : current(node) {}
