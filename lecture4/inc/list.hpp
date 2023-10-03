ac6f6d68a894aed0fef6ee1ced94cbc124726a2b
fomgleb <fomglib@gmail.com>
2023-10-03 14:43:55 +0300

    lecture4: Add the iterator for `List` class

--- lecture4/inc/list.hpp
+++ lecture4/inc/list.hpp
@@ -8,4 +8,14 @@ class List {
         Node* next;
         T data;
     }* head = nullptr;
+public:
+    struct iterator {
+        Node* current;
+        iterator(Node* node) : current(node) {}
+        bool operator!=(const iterator& other) { return current != other.current; }
+        void operator++() { current = current->next; }
+        T& operator*() { return current->data; }
+    };
+    iterator begin() { return iterator(head); }
+    iterator end() { return iterator(nullptr); }
 };
