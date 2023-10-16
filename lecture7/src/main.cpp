65f31a189fc56064dc7416871e434fce2b7afa82
fomgleb <fomglib@gmail.com>
2023-10-16 14:42:01 +0300

    lecture7: Add `BinaryTree` tests in main

--- lecture7/src/main.cpp
+++ lecture7/src/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include "binary_tree.hpp"
 
 int main() {
-    std::cout << "Test" << std::endl;
+    BinaryTree binaryTree;
+
+    binaryTree.InsertData(124);
+    binaryTree.InsertData(1);
+    binaryTree.InsertData(20);
+    binaryTree.InsertData(39);
+    binaryTree.InsertData(8);
+    binaryTree.InsertData(9);
+    binaryTree.InsertData(3);
+    binaryTree.InsertData(-18);
+    binaryTree.InsertData(299);
+    binaryTree.InsertData(35);
+    binaryTree.InsertData(143);
+    binaryTree.InsertData(311);
+    binaryTree.InsertData(2);
+
+
+    binaryTree.printTree();
+
     return 0;
 }
